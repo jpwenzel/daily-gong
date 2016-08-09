f8d8cb01a5c3bc5a652f6c96251f5f5530365487
Jean Pierre Wenzel <jeanpierre.wenzel@sophos.com>
2016-08-09 14:02:58 +0200

    Fixed crontab entry. Updated comments.

--- daily-gong.sh
+++ daily-gong.sh
@@ -4,7 +4,8 @@
 #
 # Have Big Ben invite everyone to the daily stand-up meeting.
 
-# Sleep for 45 seconds to end the chime playing just on time at 10:15.
+# Sleep for 45 seconds and then start playing the Big Ben chime just in time to
+# end at exactly 10:15.
 sleep 45
 
 /usr/bin/play /opt/local/daily-gong/bigben-15s.wav
